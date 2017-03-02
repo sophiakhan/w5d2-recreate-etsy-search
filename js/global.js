a427a436cff4d4c39a92b024d6acf1bd27c3483a
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-01 22:23:11 -0500

    (attempted to) add functionality to seach, update html css

--- js/global.js
+++ js/global.js
@@ -13,6 +13,37 @@ function getData() {
 }
 
 
+function renderItemCard(items) {
+    items.forEach(function(details){
+    var card = `<div class="col-sm-4  col-md-3">
+        <div class="card">
+        <img src="${details.Images[0].url_170x135}"/>
+        <h5 class="card-caption">${details.title.slice(0, 50)}...</h5>
+        <div class="row">
+            <h6 class="card-author col-sm-6">${details.Shop.shop_name}</h6>
+            <h6 class="card-price text-right col-sm-6">${details.price}</h6>
+        </div>
+        </div>
+    </div>`;
+    
+    document.querySelector('.itemCards').innerHTML += card; 
+    })
+};
+
+function searchFor() {
+    var searchKeyword = document.getElementById('#searchInput').addEventListener('onkeypress', innerHTML.value);
+}
+
+var searchClick = document.querySelector('#search-button').addEventListener("click", searchFor);
+
+
+
+
+
+// window.addEventListener('onkeypress', searchFor() {
+//     document.getElementById('#searchInput')
+// });
+
 // function renderItemCard() {
 //     etsy.results.forEach(createCard) 
 // }
@@ -31,20 +62,3 @@ function getData() {
 
 //     document.querySelector('.itemCards').innerHTML += card; 
 // }
-
-function renderItemCard(items) {
-    items.forEach(function(details){
-    var card = `<div class="col-sm-4  col-md-3">
-        <div class="card">
-        <img src="${details.Images[0].url_170x135}"/>
-        <h5 class="card-caption">${details.title.slice(0, 50)}...</h5>
-        <div class="row">
-            <h6 class="card-author col-sm-6">${details.Shop.shop_name}</h6>
-            <h6 class="card-price text-right col-sm-6">${details.price}</h6>
-        </div>
-        </div>
-    </div>`;
-    
-    document.querySelector('.itemCards').innerHTML += card; 
-    })
-};
